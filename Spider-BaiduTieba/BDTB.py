44a27dac7885659657989040832c32fddccea3ac
Stanley C <aumujun@users.noreply.github.com>
2017-11-26 21:59:35 +0800

    爬取百度贴吧楼主贴

---
+++ Spider-BaiduTieba/BDTB.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017-10 - aumujun <aumujun@gmail.com> 
+__author__ = 'Aumujun'
+
+import re
+import urllib.request
+import urllib.parse
+import urllib.error
+
+
+class Tool:
+    #删除img标签
+    filter_img = re.compile(r'<img class="BDE_Image" .*?>| {12}|<img pic_type=".*?>')
+    #替换br为\n加空格
+    filter_br = re.compile(r'<br><br>|<br>')
+    #删除a超链接标签
+    filter_a = re.compile(r'<a.*?>|</a>')
+    #替换td标签为t
+    filter_td = re.compile(r'<td>')
+
+    def replace(self,x):
+        x = re.sub(self.filter_img,'',x)
+        x = re.sub(self.filter_br,'\n',x)
+        x = re.sub(self.filter_a,'',x)
+        x = re.sub(self.filter_td,'\t',x)
+
+        return x.strip()
+
+
+class BDTB:
+
+    def __init__(self,baseUrl,seeLZ):
+
+        self.baseUrl = baseUrl
+        self.seeLZ = '?see_lz='+str(seeLZ)
+        self.tool = Tool()
+
+    def getPage(self,pageNum):
+        try:
+            url = self.baseUrl + self.seeLZ + '&pn=' + str(pageNum)
+            request = urllib.request.Request(url)
+            request.add_header('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
+            rsp = urllib.request.urlopen(request)
+            rsp = rsp.read().decode('utf8')
+            return rsp
+        except urllib.error.URLError as e:
+            if hasattr(e,'reason'):
+                print('连接百度贴吧失败，错误原因：',e.reason)
+                return None
+    #提取帖子的标题
+    def getTitle(self):
+        page = self.getPage(1)
+        pattern = re.compile(r'<h3 class="core_title_txt pull-left text-overflow.*?>(.*?)</h3>')
+        result = pattern.search(page)
+        if result:
+            print(result.group(1))
+        else:
+            return None
+    #获取帖子的总页数
+    def getPageNum(self):
+        page = self.getPage(1)
+        pattern = re.compile(r'<li class="l_reply_num.*?</span>.*?<span.*?>(.*?)</span>',re.S)
+        result = pattern.search(page)
+        #strip()用于移除字符串首位的字符 默认为空格
+        return result.group(1).strip()
+
+    def getContent(self,page):
+        pattern = re.compile(r'<div id="post_content_.*?>(.*?)</div>',re.S)
+        contents = pattern.findall(page)
+        louc = 1
+        for content in contents:
+            print(louc,'Floor:',"="*35+'\n')
+            print(self.tool.replace(content)+'\n')
+            louc += 1
+
+
+
+        ...
+baseURL = 'https://tieba.baidu.com/p/3138733512?see_lz=1&pn=2'
+bdtb = BDTB(baseURL,1)
+bdtb.getContent(bdtb.getPage(1))
+
+
+
+
+
+
